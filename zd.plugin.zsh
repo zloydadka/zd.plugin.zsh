ddf275b6b9ec54d2bebfa860bd20d42a4cd547c5
Alex <zloydadka@gmail.com>
2013-02-04 00:08:46 +0400

    yaourt

--- zd.plugin.zsh
+++ zd.plugin.zsh
@@ -17,5 +17,14 @@ alias gd='git diff'
 alias gc='git commit'
 alias tlog='tail -f log/development.log'
 
+alias pacman='PACMAN=/usr/bin/pacman; [ -f /usr/bin/pacman-color ] && PACMAN=/usr/bin/pacman-color; $PACMAN $@'
+
+export YAOURT_COLORS="no=0:nb=1:pkg=1;34:ver=0;32:lver=1;32:installed=0;36:grp=1;34:od=0;36:votes=0;35:dsc=0:other=0;35:testing=1;31:core=0;31:extra=0;32:xyne-any=0;36:community=0;36:aur=1;36"
+export PACMAN=pacman-color
+
+alias ls="ls --color"
+alias la="ls -a --color"
+alias .="ls -lah --color"
+
 # add plugin's bin directory to path
 export PATH="$(dirname $0)/bin:$PATH"
