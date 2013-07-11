34bbcc4ec8e4bdafe5e88788cdea9ec32239b4cb
Alexandr Andrianov <zloydadka@gmail.com>
2013-07-11 23:34:56 +0400

    open

--- zd.plugin.zsh
+++ zd.plugin.zsh
@@ -6,6 +6,11 @@ h() { cd ~/$1; }
 _h() { _files -W ~/ -/; }
 compdef _h h
 
+open() {
+  xdg-open ${1:='.'}
+}
+
+
 export EDITOR='emacs -nw'
 
 # autocorrect is more annoying than helpful
@@ -18,6 +23,8 @@ alias gc='git commit'
 alias tlog='tail -f log/development.log'
 
 alias pacman='PACMAN=/usr/bin/pacman; [ -f /usr/bin/pacman-color ] && PACMAN=/usr/bin/pacman-color; $PACMAN $@'
+alias syu="yaourt -Syua"
+
 
 export YAOURT_COLORS="no=0:nb=1:pkg=1;34:ver=0;32:lver=1;32:installed=0;36:grp=1;34:od=0;36:votes=0;35:dsc=0:other=0;35:testing=1;31:core=0;31:extra=0;32:xyne-any=0;36:community=0;36:aur=1;36"
 export PACMAN=pacman-color
@@ -27,7 +34,6 @@ alias la="ls -a --color"
 alias .="ls -lah --color"
 
 
-alias open="xdg-open"
 
 # add plugin's bin directory to path
 export PATH="$(dirname $0)/bin:$PATH"
